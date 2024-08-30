1b05d91d1a08af08ca8dcf2706be89897212b5f2
Dexter Corley <dexter.corley@live.com>
2024-08-30 14:35:58 -0700

    aug30

--- labs/lab3/lab3.sh
+++ labs/lab3/lab3.sh
@@ -1,3 +1,13 @@
-echo "first question"; cat questionone.txt | sed 's/ /,/g' | cut -d "," -f1,4 
+cat questionone.txt | grep "Success" > questionone2.txt
 
-echo "second questions"; 
+echo "first question"; cat questionone2.txt | sed 's/ /,/g' | cut -d "," -f1,4 
+
+echo "second question: 1)"; cat books.txt | grep "^[12]"
+
+echo "second question: 2)"; cat books.txt | cut -d ";" -f2 | grep -E "^\s*[JF]"
+
+echo "second question: 3)"; cat books.txt | grep "19"
+
+echo "second question: 4)"; cat books.txt | grep "[^Fiction]$"
+
+echo "second question: 5)"; cat books.txt | grep -E '(Fiction|Fantasy)$'
